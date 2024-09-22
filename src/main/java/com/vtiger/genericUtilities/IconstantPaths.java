fa70cbbaeef4a3ef2d5da783248b71645f287c88
Harsh Kumar Sharma <71114819+Hks1998@users.noreply.github.com>
2024-09-22 20:07:06 +0530

    Update IconstantPaths.java

--- src/main/java/com/vtiger/genericUtilities/IconstantPaths.java
+++ src/main/java/com/vtiger/genericUtilities/IconstantPaths.java
@@ -1,8 +1,8 @@
 package com.vtiger.genericUtilities;
 
 public interface IconstantPaths {
-	String propertyFilepath="C:\\Users\\harsh\\Downloads\\eclipse-workspace-2a\\eclipse-workspace-2\\Framework_sel3\\src\\test\\resources\\CommonData.properties";
-	String excelFilepath="C:\\Users\\harsh\\Downloads\\eclipse-workspace-2a\\eclipse-workspace-2\\Framework_sel3\\src\\test\\resources\\commonData.xlsx";
+	String propertyFilepath="C:\\Users\\harsh\\git\\Framework_Selenium\\src\\test\\resources\\CommonData.properties";
+	String excelFilepath="C:\\Users\\harsh\\git\\Framework_Selenium\\src\\test\\resources\\commonData.xlsx";
 
 
 }
