6debd93ae9a856a838f3a99648079f2f3d78d712
harshkrsharma <harshkrsharma@kali>
2024-09-23 23:20:26 +0530

    Updated pom.xml with maven compiler plugin

--- src/main/java/com/vtiger/genericUtilities/IconstantPaths.java
+++ src/main/java/com/vtiger/genericUtilities/IconstantPaths.java
@@ -1,8 +1,8 @@
 package com.vtiger.genericUtilities;
 
 public interface IconstantPaths {
-	String propertyFilepath="C:\\Users\\harsh\\git\\Framework_Selenium\\src\\test\\resources\\CommonData.properties";
-	String excelFilepath="C:\\Users\\harsh\\git\\Framework_Selenium\\src\\test\\resources\\commonData.xlsx";
+	String propertyFilepath="/home/harshkrsharma/git/Framework_Selenium/src/test/resources/CommonData.properties";
+	String excelFilepath="/home/harshkrsharma/git/Framework_Selenium/src/test/resources/commonData.xlsx";
 
 
 }
