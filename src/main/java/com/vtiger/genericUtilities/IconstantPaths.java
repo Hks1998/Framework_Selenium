891239acb3154b098e20fe4ef3a824c3a4572821
Harsh Kumar Sharma <harshkumarbvm@gmail.com>
2024-09-25 16:40:36 +0530

    Updated selenium version to 4.25.0

--- src/main/java/com/vtiger/genericUtilities/IconstantPaths.java
+++ src/main/java/com/vtiger/genericUtilities/IconstantPaths.java
@@ -1,8 +1,8 @@
 package com.vtiger.genericUtilities;
 
 public interface IconstantPaths {
-	String propertyFilepath="/home/harshkrsharma/Documents/eclipse-workspace/Framework_Selenium/src/test/resources/CommonData.properties";
-	String excelFilepath="/home/harshkrsharma/Documents/eclipse-workspace/Framework_Selenium/src/test/resources/commonData.xlsx";
+	String propertyFilepath="/home/harshkrsharma/IdeaProjects/Framework_Selenium/src/test/resources/CommonData.properties";
+	String excelFilepath="/home/harshkrsharma/IdeaProjects/Framework_Selenium/src/test/resources/commonData.xlsx";
 
 
 }
